99aff2c29414f4bc1d33b74f4e835f14d364851b
Mrchado <mongochadrac@gmail.com>
2026-05-22 09:04:06 +0100

    Phase finale 220526 v0

--- config/db.sql
+++ config/db.sql
@@ -1,9 +1,6 @@
-
 IF NOT EXISTS (
-    SELECT name 
-    FROM sys.databases 
-    WHERE name = N'MTCAISSEWEB'
+    SELECT name FROM sys.databases WHERE name = N'MTCAISSEWEB'
 )
 BEGIN
-    CREATE DATABASE [MTCAISSEWEB];
+    CREATE DATABASE [MTCAISSEWEB]
 END
