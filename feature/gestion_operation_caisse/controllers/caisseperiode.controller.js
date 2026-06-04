9ef5d8df20963ae2aee2b69b2d058926a1661c1a
Mrchado <mongochadrac@gmail.com>
2026-06-04 18:35:32 +0100

    new commit v0

--- feature/gestion_operation_caisse/controllers/caisseperiode.controller.js
+++ feature/gestion_operation_caisse/controllers/caisseperiode.controller.js
@@ -132,8 +132,18 @@ module.exports.get_recentperiode = asyncHandler(async(req, res, next) => {
   }
 });
 
+module.exports.get_caisse_tresorerie_by_date = asyncHandler(async(req, res, next) => {
+  try {
+    const { startDate, endDate, idcaisse } = req.body;
+    const data = await caisseperiodeservice.get_caisse_tresorerie_by_date(startDate, endDate, idcaisse);
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(404).json({ success: false, message: error.message });
+  }
+});
+
 /**
- * Validation une periode caisse existante
+ * Recalculate le solde d'une periode caisse existante
  */
 module.exports.recalculate_solde = asyncHandler(async(req, res, next) => {
   try {
